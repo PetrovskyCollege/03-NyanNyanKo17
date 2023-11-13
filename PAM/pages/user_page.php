5bcdcc1384f7cfb2db3ee00f8749589c38e09f2b
Ника <113297706+NyanNyanKo17@users.noreply.github.com>
2023-11-13 13:10:20 +0300

    Update user_page.php

--- PAM/pages/user_page.php
+++ PAM/pages/user_page.php
@@ -6,6 +6,27 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    require_once '../assets/php/database.php';  // Подключите файл с классом для работы с базой данных
+
+    $db = new Database();
+    $user_id = $_SESSION['user_id'];
+
     
+    $user = $db->getUserById($user_id);
+
+    if ($user) {
+        $username = $user['login'];
+        echo "Привет, $username!";
+    } else {
+        echo "Ошибка: Не удалось получить информацию о пользователе.";
+    }
+} else {
+    echo "Вы не авторизованы.";
+}
+?>
 </body>
 </html>
